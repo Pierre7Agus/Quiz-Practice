91fb478e0655df1dd954a7b0f2dd224db3c61b26
Pierre7Agus <redtor_90@outlook.es>
2021-11-01 15:24:36 -0500

    Add a constant in question.js the constant receives a new array, where each element is a Question object and it receives the corresponding values from data.js

---
+++ data/question.js
@@ -0,0 +1,6 @@
+import {data} from './data.js';
+import {Question} from './models/Question.js';
+
+export const questions = data.map(item=>{
+    return new Question(item.question,item.choices,item.answer);
+});
