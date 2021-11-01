0a956a120b3f0dfeeda98c39c160831ba8daae26
Pierre7Agus <redtor_90@outlook.es>
2021-11-01 15:35:27 -0500

    Add Quiz class in Quiz.js Adding two variables, the score variable allows me to handle the user's score, and the index variable allows me to handle the indices of the array. Configure the constructor which receives a parameter.

---
+++ models/Quiz.js
@@ -0,0 +1,14 @@
+class Quiz{
+
+    score=0;
+    index=0;
+
+    /**
+     * 
+     * @param {[Question]} questions it receives an array, where each element is an object Question
+     */
+
+    constructor(questions){
+        this.questions=questions;
+    }
+}
