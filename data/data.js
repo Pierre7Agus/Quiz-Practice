f97c48886cd9454c6f9fb9a116eaf967d2f3d465
Pierre7Agus <redtor_90@outlook.es>
2021-11-01 14:51:45 -0500

    Add data in data.js An array with 4 elements was added, where each element is an object. This information will be shown to the user.

---
+++ data/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        question : 'Which of the following is a programming languaje?',
+        choices: ['HTML','CSS','Coursera','JavaScript'],
+        answer: 'JavaScript'
+    },
+    {
+        question: 'Which of the following is a low-level programming language?' ,
+        choices: ['Python','Adobe','Scratch','Cobol'],
+        answer: 'Cobol'
+    },
+    {
+        question:'Inside which element is the content of a web page added?'  ,
+        choices:['body','head','img','script'],
+        answer:'body'
+    },
+    {
+        question: 'Which of the following is a code editor?' ,
+        choices:['Moon','SaaS','Atom','Scrum'],
+        answer:'Atom'
+    },
+];
