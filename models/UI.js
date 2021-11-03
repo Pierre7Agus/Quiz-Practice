5d9106be91b7f0620bf822567e6803b1f9b07842
Pierre7Agus <redtor_90@outlook.es>
2021-11-02 22:39:17 -0500

    Add showChoices method in the UI class This method allows me to show the possible answers of the quiz

--- models/UI.js
+++ models/UI.js
@@ -1,11 +1,21 @@
-import Question from './Question.js';
-import Quiz from './Quiz.js';
-
 class UI{
     constructor(){}
 
     showQuestion(text){
         const question = document.getElementById('title');
         question.innerText = text;
+    }
+
+
+    showChoices(choices, callback){
+        const options = document.getElementById('choices');
+        choices.forEach((elemento)=>{
+            const button = document.createElement('button');
+            button.innerText= elemento;
+            button.addEventListener('click',(event)=>{
+                callback(event.target.innerText);
+            });
+            options.append(button);
+        });
     }
 }
